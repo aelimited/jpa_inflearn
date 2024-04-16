c16500863a680f361f3c9bed04493b849d80095d
aelimited <cloim09@gmail.com>
2024-04-16 18:39:44 +0900

    Add Test

---
+++ src/test/java/com/example/datajpa/Repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.datajpa.Repository;
+
+import com.example.datajpa.entity.Member;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member saveMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(saveMember.getId());
+
+        assertEquals(member.getId(), findMember.getId());
+        assertEquals(saveMember, findMember);
+
+    }
+}
---
+++ src/test/java/com/example/datajpa/Repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.example.datajpa.Repository;
+
+import com.example.datajpa.entity.Member;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("Member A");
+        Member saveMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(saveMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+    }
+}
